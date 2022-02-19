d8f87c7b50f4f35cae69d8615d4c05c625bb361a
yshaikh165 <65322529+yshaikh165@users.noreply.github.com>
2022-02-19 00:11:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,24 +13,31 @@
 #include <cmath>
 
 int main() {
-  BMP bmpNew(1500,800,false);  
+  BMP bmpNew(1500,800,false);  //This creates canvas with dimensions of 1500 by 800
   bmpNew.fill_region(0, 0, 200, 200, 0, 0, 0, 0);
 
   double x = 0; 
   double y = 0;
+  
+  //The power function is used because of the equation that was given
   x = pow(64, 2.0); 
   y = pow(12.0, 2.0);
-   
+  
+  //Iterating through for loop to create the ellipse 
   for(int i=0;i<bmpNew.bmp_info_header.width;i++)
   {
+    //Each calculation is done on a seperate line to ensure readability
+    //Shifts image
     double z = 0;
-    z = pow(i-750,2.0);
+    z = pow(i-750,2.0); 
     z = z/y;
     z = x-z;
     z = sqrt(z);
     z = 6*z;
     
+    //RGB
     bmpNew.set_pixel(i, z, 255, 250, 250, 255); 
   }
+  //Creates bmp file to display picture
   bmpNew.write("output.bmp");
 }
