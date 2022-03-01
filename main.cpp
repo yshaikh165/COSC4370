fd9a86e6db325a76eace438c1f58871fe64e6975
yshaikh165 <65322529+yshaikh165@users.noreply.github.com>
2022-03-01 11:33:25 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,152 @@
-/* Sources:
--https://solarianprogrammer.com/2018/11/19/cpp-reading-writing-bmp-images/
--https://www.geeksforgeeks.org/midpoint-ellipse-drawing-algorithm/
--https://www.geeksforgeeks.org/draw-ellipse-c-graphics/?ref=lbp
--https://www.geeksforgeeks.org/basic-graphic-programming-in-c/?ref=lbp
--https://www.cplusplus.com/doc/tutorial/functions/
--https://stackoverflow.com/questions/5886628/effecient-way-to-draw-ellipse-with-opengl-or-d3d
--https://www.codeproject.com/Questions/1239612/Drawing-an-ellipse-using-Cplusplus-with-opengl
-*/
+/*******************************************************
+ * Homework 2: OpenGL                                  *
+ *-----------------------------------------------------*
+ * First, you should fill in problem1(), problem2(),   *
+ * and problem3() as instructed in the written part of *
+ * the problem set.  Then, express your creativity     *
+ * with problem4()!                                    *
+ *                                                     *
+ * Note: you will only need to add/modify code where   *
+ * it says "TODO".                                     *
+ *                                                     *
+ * The left mouse button rotates, the right mouse      *
+ * button zooms, and the keyboard controls which       *
+ * problem to display.                                 *
+ *                                                     *
+ * For Linux/OS X:                                     *
+ * To compile your program, just type "make" at the    *
+ * command line.  Typing "make clean" will remove all  *
+ * computer-generated files.  Run by typing "./hw2"    *
+ *                                                     *
+ * For Visual Studio:                                  *
+ * You can create a project with this main.cpp and     *
+ * build and run the executable as you normally would. *
+ *******************************************************/
 
 #include <iostream>
-#include "BMP.h"
 #include <cmath>
+#include <cstdio>
+#include <cstdlib>
 
-int main() {
-  BMP bmpNew(1500,800,false);  //This creates canvas with dimensions of 1500 by 800
-  bmpNew.fill_region(0, 0, 200, 200, 0, 0, 0, 0);
+#include "./freeglut-3.2.1/include/GL/freeglut.h"
 
-  double x = 0; 
-  double y = 0;
-  
-  //The power function is used because of the equation that was given
-  x = pow(64, 2.0); 
-  y = pow(12.0, 2.0);
-  
-  //Iterating through for loop to create the ellipse 
-  for(int i=0;i<bmpNew.bmp_info_header.width;i++)
-  {
-    //Each calculation is done on a seperate line to ensure readability
-    //Shifts image
-    double z = 0;
-    z = pow(i-750,2.0); 
-    z = z/y;
-    z = x-z;
-    z = sqrt(z);
-    z = 6*z; //quadrant
-    
-    //RGB
-    bmpNew.set_pixel(i, z, 255, 250, 250, 255); 
-  }
-  //Creates bmp file to display picture
-  bmpNew.write("output.bmp");
+using namespace std;
+
+bool leftDown = false, rightDown = false;
+int lastPos[2];
+float cameraPos[4] = {0,1,4,1};
+int windowWidth = 640, windowHeight = 480;
+double yRot = 0;
+int curProblem = 1; // TODO: change this number to try different examples
+
+float specular[] = { 1.0, 1.0, 1.0, 1.0 };
+float shininess[] = { 50.0 };
+
+void problem1() {
+    // TODO: Your code here!
+}
+
+void problem2() {
+    // TODO: Your code here!
+}
+
+void problem3() {
+    // TODO: Your code here!
+}
+
+void problem4() {
+    // TODO: Your code here!
+}
+
+void display() {
+	glClearColor(0,0,0,0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glDisable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glBegin(GL_LINES);
+		glColor3f(1,0,0); glVertex3f(0,0,0); glVertex3f(1,0,0); // x axis
+		glColor3f(0,1,0); glVertex3f(0,0,0); glVertex3f(0,1,0); // y axis
+		glColor3f(0,0,1); glVertex3f(0,0,0); glVertex3f(0,0,1); // z axis
+	glEnd(/*GL_LINES*/);
+
+	glEnable(GL_LIGHTING);
+	glShadeModel(GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+	glEnable(GL_LIGHT0);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0,0,windowWidth,windowHeight);
+
+	float ratio = (float)windowWidth / (float)windowHeight;
+	gluPerspective(50, ratio, 1, 1000);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(cameraPos[0], cameraPos[1], cameraPos[2], 0, 0, 0, 0, 1, 0);
+
+	glLightfv(GL_LIGHT0, GL_POSITION, cameraPos);
+
+	glRotatef(yRot,0,1,0);
+
+	if (curProblem == 1) problem1();
+	if (curProblem == 2) problem2();
+	if (curProblem == 3) problem3();
+	if (curProblem == 4) problem4();
+
+	glutSwapBuffers();
+}
+
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_LEFT_BUTTON) leftDown = (state == GLUT_DOWN);
+	else if (button == GLUT_RIGHT_BUTTON) rightDown = (state == GLUT_DOWN);
+
+	lastPos[0] = x;
+	lastPos[1] = y;
+}
+
+void mouseMoved(int x, int y) {
+	if (leftDown) yRot += (x - lastPos[0])*.1;
+	if (rightDown) {
+		for (int i = 0; i < 3; i++)
+			cameraPos[i] *= pow(1.1,(y-lastPos[1])*.1);
+	}
+
+
+	lastPos[0] = x;
+	lastPos[1] = y;
+	glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	curProblem = key-'0';
+    if (key == 'q' || key == 'Q' || key == 27){
+        exit(0);
+    }
+	glutPostRedisplay();
+}
+
+void reshape(int width, int height) {
+	windowWidth = width;
+	windowHeight = height;
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutCreateWindow("HW2");
+
+	glutDisplayFunc(display);
+	glutMotionFunc(mouseMoved);
+	glutMouseFunc(mouse);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+
+	glutMainLoop();
+
+	return 0;
 }
