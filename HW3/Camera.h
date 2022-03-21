83fe0f13f9be532f6ee568c45af9e59a45b7cf44
yshaikh165 <65322529+yshaikh165@users.noreply.github.com>
2022-03-20 23:08:56 -0500

    Update Camera.h

--- HW3/Camera.h
+++ HW3/Camera.h
@@ -50,7 +50,7 @@ public:
     }
 
     // TODO: Returns the view matrix calculated using Eular Angles and the LookAt Matrix
-    glm::myMatrix GetViewMatrix()
+    glm::mat4 GetViewMatrix()
     {
       return glm::lookAt(Position, Position + Front, Up);
     }
